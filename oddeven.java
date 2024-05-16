57e781a4ec6a35fb776e6dd797f03dd2d0d05830
my-new-code <160898akshay@gmail.com>
2024-05-16 12:09:53 +0530

    message

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+
+public class Oddeven {
+
+    public static void main(String[] args) {
+        int number = 20;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
